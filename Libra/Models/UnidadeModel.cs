82762ea97580cc4efdc2deb11c363f1299dde05b
Aline e Kelvin Lima <Aline e Kelvin Lima>
2016-11-26 10:41:36 -0200

    Inicial

---
+++ Libra/Models/UnidadeModel.cs
@@ -0,0 +1,32 @@
+﻿using Libra.Communs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Libra.Models
+{
+    [Table("Unidade")]
+    public class UnidadeModel
+    {
+        public int UnidadeId { get; set; }
+        public string Nome { get; set; }
+        public string Apelido { get; set; }
+        public string UF { get; set; }
+        public string Cidade { get; set; }
+        public string Bairro { get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string CEP { get; set; }
+        public string Telefone1 { get; set; }
+        public string Telefone2 { get; set; }
+        public string Fax { get; set; }
+        public string Email1 { get; set; }
+        public string Email2 { get; set; }
+        public string Observacao { get; set; }
+        public bool Ativo { get; set; }
+        public TipoUnidade TipoUnidade { get; set; }
+    }
+}
